ae37de5f43692f6633dd7d06d4c467fbf4692131
Reginald <nksreg002@myuct.ac.za>
2025-06-26 20:49:44 +0200

    added  a name change of the app and a discription

--- src/App.js
+++ src/App.js
@@ -189,8 +189,11 @@ const App = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg">
       <h1 className="text-2xl font-bold text-center mb-6">
-        Process Value to 4-20mA Calculator
+      LoopMate
       </h1>
+      <h2>
+      Your quick companion for resistor values and signal simulation.
+      </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Left side controls */}
